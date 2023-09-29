c0a71f221904284dbb05d784eb7702b8e63eb359
alsrlqor <skyup9393@gmail.com>
2023-09-30 02:05:06 +0900

    feat: wip-21 Admin 초기 모델 추가

---
+++ src/database/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Admin(Base):
+    __tablename__ = "admin"
+
+    id = Column(Integer, primary_key=True, index=True)
+    login_name = Column(String(256), unique=True, nullable=False)
+    password = Column(String(256), nullable=False)
+    nickname = Column(String(256), unique=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
+    updated_at = Column(
+        DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
+    )
+
+    def __repr__(self):
+        return f"Admin(id={self.id}, login_name={self.login_name}, nickname={self.nickname})"
