1e62eb717f7a39b234077e078e6f6da8efe5fa65
alsrlqor <skyup9393@gmail.com>
2023-10-16 01:13:01 +0900

    feat: wip-29 refresh token 스키마 추가 및 수정

--- src/admin/schema/signup_response.py
+++ src/admin/schema/signup_response.py
@@ -5,4 +5,4 @@ from admin.schema.admin_info_response import AdminInfoResponse
 
 class SignUpResponse(BaseModel):
     data: AdminInfoResponse
-    message: str = "Created new admin account successfully."
+    detail: str = "Created new admin account successfully."
