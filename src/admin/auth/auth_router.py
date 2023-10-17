d68c3b43b70612fb62f2268a95ea74cd8a4d3426
alsrlqor <skyup9393@gmail.com>
2023-10-17 22:51:58 +0900

    feat: wip-30 이메일 인증 API 및 서비스 로직 추가

--- src/admin/auth/auth_router.py
+++ src/admin/auth/auth_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Path
+from fastapi import APIRouter, Depends, Path, Query
 from starlette.status import (
     HTTP_201_CREATED,
     HTTP_400_BAD_REQUEST,
@@ -6,16 +6,25 @@ from starlette.status import (
     HTTP_200_OK,
     HTTP_401_UNAUTHORIZED,
     HTTP_404_NOT_FOUND,
+    HTTP_500_INTERNAL_SERVER_ERROR,
 )
 
+from admin.auth.smtp_manager import SmtpManager
 from admin.schema.login_request import LogInRequest
 from admin.schema.login_response import LoginResponse
+from admin.schema.auth_num_response import SendAuthNumResponse, VerifyAuthNumResponse
 from admin.schema.signup_response import SignUpResponse
 from admin.schema.token_response import RefreshTokensResponse
 from admin.security import get_token
 from database.repositories.admin_repository import AdminRepository
 from admin.auth.auth_manager import AuthManager
-from admin.auth.auth_service import create_admin, admin_login, refresh_access_token
+from admin.auth.auth_service import (
+    create_admin,
+    admin_login,
+    refresh_access_token,
+    send_auth_number_email,
+    verify_email_auth_number,
+)
 from admin.schema.signup_request import SignUpRequest
 
 router = APIRouter()
@@ -53,6 +62,7 @@ async def admin_login_handler(
     request: LogInRequest,
     auth_manager: AuthManager = Depends(),
     admin_repository: AdminRepository = Depends(),
+    smtp_manager: SmtpManager = Depends(),
 ):
     return await admin_login(**locals())
 
@@ -72,5 +82,43 @@ async def refresh_handler(
     token: str = Depends(get_token),
     auth_manager: AuthManager = Depends(),
     admin_repository: AdminRepository = Depends(),
+    smtp_manager: SmtpManager = Depends(),
 ) -> RefreshTokensResponse:
     return await refresh_access_token(**locals())
+
+
+@router.post(
+    "/email/authorization/{email}",
+    status_code=HTTP_201_CREATED,
+    responses={
+        HTTP_201_CREATED: {
+            "description": "Send email with auth number and saved in-memory data successfully"
+        },
+        HTTP_500_INTERNAL_SERVER_ERROR: {"description": "Redis error occurred"},
+    },
+    summary="인증번호 이메일 발송",
+)
+async def send_signup_email_handler(
+    email: str = Path(..., description="가입 대기 이메일"),
+    smtp_manager: SmtpManager = Depends(),
+) -> SendAuthNumResponse:
+    return await send_auth_number_email(**locals())
+
+
+@router.get(
+    "/email/verification",
+    status_code=HTTP_200_OK,
+    responses={
+        HTTP_200_OK: {"description": "Passed email verification with auth number"},
+        HTTP_400_BAD_REQUEST: {"description": "Wrong auth number"},
+        HTTP_404_NOT_FOUND: {"description": "No auth number with the requested email"},
+        HTTP_500_INTERNAL_SERVER_ERROR: {"description": "Redis error occurred"},
+    },
+    summary="이메일 인증번호 검증",
+)
+async def email_authorization_handler(
+    email: str = Query(..., description="이메일"),
+    auth_number: int = Query(..., description="검증 번호"),
+    smtp_manager: SmtpManager = Depends(),
+) -> VerifyAuthNumResponse:
+    return await verify_email_auth_number(**locals())
