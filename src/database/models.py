3e1b10ef5e9bb558299e27631baa4dbf7e50fa93
alsrlqor <skyup9393@gmail.com>
2023-10-23 01:50:21 +0900

    feat: wip-32 의원 관련 DB 모델 추가

--- src/database/models.py
+++ src/database/models.py
@@ -1,7 +1,17 @@
 from datetime import datetime
+from typing import List
 
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    JSON,
+    ForeignKey,
+    Boolean,
+    Table,
+)
+from sqlalchemy.orm import declarative_base, relationship, Mapped, mapped_column
 
 Base = declarative_base()
 
@@ -9,7 +19,7 @@ Base = declarative_base()
 class Admin(Base):
     __tablename__ = "admin"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
     email = Column(String(256), unique=True, nullable=False)
     password = Column(String(256), nullable=False)
     nickname = Column(String(256), unique=True, nullable=False)
@@ -33,3 +43,160 @@ class Admin(Base):
         self.hashed_refresh_token = hashed_refresh_token
         self.uuid_jti = uuid_jti
         return self
+
+
+class Politician(Base):
+    __tablename__ = "politician"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name = Column(String(256), nullable=False, comment="의원 이름")
+    profile_url = Column(String(256), nullable=True, comment="프로필 주소")
+    political_party = Column(String(256), nullable=False, comment="소속 정당")
+    elected_count = Column(Integer, nullable=False, comment="당선 횟수")
+    total_promise_count = Column(Integer, nullable=True, comment="총 공약수")
+    completed_promise_count = Column(Integer, nullable=True, comment="완료 공약수")
+    in_progress_promise_count = Column(Integer, nullable=True, comment="추진 중인 공약수")
+    pending_promise_count = Column(Integer, nullable=True, comment="보류 공약수")
+    discarded_promise_count = Column(Integer, nullable=True, comment="폐기 공약수")
+    other_promise_count = Column(Integer, nullable=True, comment="기타 공약수")
+    resolve_required_promise_count = Column(
+        Integer, nullable=True, comment="필요 입법 공약 총 수"
+    )
+    resolved_promise_count = Column(Integer, nullable=True, comment="입법 의결 완료 공약 총 수")
+    total_required_funds = Column(Integer, nullable=True, comment="필요 재정 총액")
+    total_secured_funds = Column(Integer, nullable=True, comment="확보 재정 총액")
+    total_executed_funds = Column(Integer, nullable=True, comment="집행 재정 총액")
+    notes = Column(JSON, nullable=True)
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
+    updated_at = Column(
+        DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
+    )
+
+    promise_count_detail: Mapped["PromiseCountDetail"] = relationship(
+        back_populates="politician"
+    )
+    committee: Mapped[List["Committee"]] = relationship(back_populates="politician")
+    jurisdiction: Mapped[List["Jurisdiction"]] = relationship(
+        back_populates="politician"
+    )
+
+    def __repr__(self):
+        return f"Politician(id={self.id}, name={self.name}, political_party={self.political_party})"
+
+
+class PromiseCountDetail(Base):
+    __tablename__ = "promise_count_detail"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    politician_id: Mapped[int] = mapped_column(
+        ForeignKey("politician.id"), nullable=False, comment="의원 id"
+    )
+    completed_national_promise_count = Column(
+        Integer, nullable=True, comment="완료 국정 공약수"
+    )
+    total_national_promise_count = Column(Integer, nullable=True, comment="총 국정 공약수")
+    completed_local_promise_count = Column(Integer, nullable=True, comment="완료 지역 공약수")
+    total_local_promise_count = Column(Integer, nullable=True, comment="총 지역 공약수")
+    completed_legislative_promise_count = Column(
+        Integer, nullable=True, comment="완료 입법 공약수"
+    )
+    total_legislative_promise_count = Column(Integer, nullable=True, comment="총 입법 공약수")
+    completed_financial_promise_count = Column(
+        Integer, nullable=True, comment="완료 재정 공약수"
+    )
+    total_financial_promise_count = Column(Integer, nullable=True, comment="총 재정 공약수")
+    completed_in_term_promise_count = Column(
+        Integer, nullable=True, comment="완료 임기 내 공약수"
+    )
+    total_in_term_promise_count = Column(Integer, nullable=True, comment="총 임기 내 공약수")
+    completed_after_term_promise_count = Column(
+        Integer, nullable=True, comment="완료 임기 후 공약수"
+    )
+    total_after_term_promise_count = Column(
+        Integer, nullable=True, comment="총 임기 후 공약수"
+    )
+    completed_ongoing_business_promise_count = Column(
+        Integer, nullable=True, comment="완료 지속 사업 공약수"
+    )
+    total_ongoing_business_promise_count = Column(
+        Integer, nullable=True, comment="총 지속 사업 공약수"
+    )
+    completed_new_business_promise_count = Column(
+        Integer, nullable=True, comment="완료 신규 사업 공약수"
+    )
+    total_new_business_promise_count = Column(
+        Integer, nullable=True, comment="총 신규 사업 공약수"
+    )
+    notes = Column(JSON, nullable=True)
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
+    updated_at = Column(
+        DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
+    )
+
+    politician: Mapped["Politician"] = relationship(
+        back_populates="promise_count_detail"
+    )
+
+    def __repr__(self):
+        return f"PromiseCountDetail(id={self.id}, politician_id={self.politician_id})"
+
+
+class Committee(Base):
+    __tablename__ = "committee"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    politician_id: Mapped[int] = mapped_column(
+        ForeignKey("politician.id"), nullable=False, comment="의원 id"
+    )
+    is_main = Column(Boolean, nullable=False, comment="해당 의원의 대표 위원회 여부")
+    name = Column(String(256), nullable=False, comment="위원회 명칭")
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
+    updated_at = Column(
+        DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
+    )
+
+    politician: Mapped["Politician"] = relationship(back_populates="committee")
+
+    def __repr__(self):
+        return f"Committee(id={self.id}, name={self.name}, politician_id={self.politician_id})"
+
+
+class Constituency(Base):
+    __tablename__ = "constituency"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    assembly_term = Column(Integer, nullable=False, comment="국회 회기")
+    region = Column(String(256), nullable=False, comment="지역구")
+    district = Column(String(256), nullable=True, comment="세부 지역구")
+    section = Column(String(256), nullable=True, comment="분구")
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
+    updated_at = Column(
+        DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
+    )
+
+    jurisdiction: Mapped["Jurisdiction"] = relationship(back_populates="constituency")
+
+    def __repr__(self):
+        return f"Constituency(id={self.id}, assembly_term={self.assembly_term}, region={self.region}, district={self.district}, section={self.section})"
+
+
+class Jurisdiction(Base):
+    __tablename__ = "jurisdiction"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    politician_id: Mapped[int] = mapped_column(
+        ForeignKey("politician.id"), nullable=False, comment="의원 id"
+    )
+    constituency_id: Mapped[int] = mapped_column(
+        ForeignKey("constituency.id"), nullable=False, comment="지역구 id"
+    )
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
+    updated_at = Column(
+        DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
+    )
+
+    politician: Mapped["Politician"] = relationship(back_populates="jurisdiction")
+    constituency: Mapped["Constituency"] = relationship(back_populates="jurisdiction")
+
+    def __repr__(self):
+        return f"Constituency(id={self.id}, politician_id={self.politician_id}, constituency_id={self.constituency_id})"
