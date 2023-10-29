c3aff5300a642b69af8acab7b38f8457df5e728b
alsrlqor <skyup9393@gmail.com>
2023-10-29 21:44:16 +0900

    feat: wip-32 의원 관련 모델 및 마이그레이션 수정해서 추가

--- src/database/models.py
+++ src/database/models.py
@@ -9,14 +9,26 @@ from sqlalchemy import (
     JSON,
     ForeignKey,
     Boolean,
-    Table,
 )
-from sqlalchemy.orm import declarative_base, relationship, Mapped, mapped_column
+from sqlalchemy.orm import (
+    declarative_base,
+    relationship,
+    Mapped,
+    mapped_column,
+    MappedAsDataclass,
+)
 
 Base = declarative_base()
 
 
-class Admin(Base):
+class DateTimeMixin:
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
+    updated_at = Column(
+        DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
+    )
+
+
+class Admin(Base, DateTimeMixin):
     __tablename__ = "admin"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
@@ -25,10 +37,6 @@ class Admin(Base):
     nickname = Column(String(256), unique=True, nullable=False)
     hashed_refresh_token = Column(String(256), nullable=True)
     uuid_jti = Column(String(256), nullable=True)
-    created_at = Column(DateTime, default=datetime.now, nullable=False)
-    updated_at = Column(
-        DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
-    )
 
     def __repr__(self):
         return f"Admin(id={self.id}, email={self.email}, nickname={self.nickname})"
@@ -45,13 +53,13 @@ class Admin(Base):
         return self
 
 
-class Politician(Base):
+class Politician(Base, DateTimeMixin):
     __tablename__ = "politician"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    name = Column(String(256), nullable=False, comment="의원 이름")
+    name = Column(String(10), nullable=False, comment="의원 이름")
     profile_url = Column(String(256), nullable=True, comment="프로필 주소")
-    political_party = Column(String(256), nullable=False, comment="소속 정당")
+    political_party = Column(String(30), nullable=False, comment="소속 정당")
     elected_count = Column(Integer, nullable=False, comment="당선 횟수")
     total_promise_count = Column(Integer, nullable=True, comment="총 공약수")
     completed_promise_count = Column(Integer, nullable=True, comment="완료 공약수")
@@ -67,10 +75,6 @@ class Politician(Base):
     total_secured_funds = Column(Integer, nullable=True, comment="확보 재정 총액")
     total_executed_funds = Column(Integer, nullable=True, comment="집행 재정 총액")
     notes = Column(JSON, nullable=True)
-    created_at = Column(DateTime, default=datetime.now, nullable=False)
-    updated_at = Column(
-        DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
-    )
 
     promise_count_detail: Mapped["PromiseCountDetail"] = relationship(
         back_populates="politician"
@@ -84,7 +88,7 @@ class Politician(Base):
         return f"Politician(id={self.id}, name={self.name}, political_party={self.political_party})"
 
 
-class PromiseCountDetail(Base):
+class PromiseCountDetail(Base, DateTimeMixin):
     __tablename__ = "promise_count_detail"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
@@ -128,10 +132,6 @@ class PromiseCountDetail(Base):
         Integer, nullable=True, comment="총 신규 사업 공약수"
     )
     notes = Column(JSON, nullable=True)
-    created_at = Column(DateTime, default=datetime.now, nullable=False)
-    updated_at = Column(
-        DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
-    )
 
     politician: Mapped["Politician"] = relationship(
         back_populates="promise_count_detail"
@@ -141,7 +141,7 @@ class PromiseCountDetail(Base):
         return f"PromiseCountDetail(id={self.id}, politician_id={self.politician_id})"
 
 
-class Committee(Base):
+class Committee(Base, DateTimeMixin):
     __tablename__ = "committee"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
@@ -149,11 +149,7 @@ class Committee(Base):
         ForeignKey("politician.id"), nullable=False, comment="의원 id"
     )
     is_main = Column(Boolean, nullable=False, comment="해당 의원의 대표 위원회 여부")
-    name = Column(String(256), nullable=False, comment="위원회 명칭")
-    created_at = Column(DateTime, default=datetime.now, nullable=False)
-    updated_at = Column(
-        DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
-    )
+    name = Column(String(30), nullable=False, comment="위원회 명칭")
 
     politician: Mapped["Politician"] = relationship(back_populates="committee")
 
@@ -161,26 +157,37 @@ class Committee(Base):
         return f"Committee(id={self.id}, name={self.name}, politician_id={self.politician_id})"
 
 
-class Constituency(Base):
+class Region(Base, DateTimeMixin):
+    __tablename__ = "region"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    region = Column(String(20), nullable=False, comment="지역구")
+
+    constituency: Mapped[List["Constituency"]] = relationship(back_populates="region")
+
+    def __repr__(self):
+        return f"Region(id={self.id}, region={self.region})"
+
+
+class Constituency(Base, DateTimeMixin):
     __tablename__ = "constituency"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     assembly_term = Column(Integer, nullable=False, comment="국회 회기")
-    region = Column(String(256), nullable=False, comment="지역구")
-    district = Column(String(256), nullable=True, comment="세부 지역구")
-    section = Column(String(256), nullable=True, comment="분구")
-    created_at = Column(DateTime, default=datetime.now, nullable=False)
-    updated_at = Column(
-        DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
+    region_id: Mapped[int] = mapped_column(
+        ForeignKey("region.id"), nullable=False, comment="지역구 id"
     )
+    district = Column(String(20), nullable=True, comment="세부 지역구")
+    section = Column(String(20), nullable=True, comment="분구")
 
+    region: Mapped["Region"] = relationship(back_populates="constituency")
     jurisdiction: Mapped["Jurisdiction"] = relationship(back_populates="constituency")
 
     def __repr__(self):
         return f"Constituency(id={self.id}, assembly_term={self.assembly_term}, region={self.region}, district={self.district}, section={self.section})"
 
 
-class Jurisdiction(Base):
+class Jurisdiction(Base, DateTimeMixin):
     __tablename__ = "jurisdiction"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
@@ -189,10 +196,6 @@ class Jurisdiction(Base):
     )
     constituency_id: Mapped[int] = mapped_column(
         ForeignKey("constituency.id"), nullable=False, comment="지역구 id"
-    )
-    created_at = Column(DateTime, default=datetime.now, nullable=False)
-    updated_at = Column(
-        DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
     )
 
     politician: Mapped["Politician"] = relationship(back_populates="jurisdiction")
