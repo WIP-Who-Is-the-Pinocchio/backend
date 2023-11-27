67c6aa5e8f11544c855d6afd296911cb03932ee9
alsrlqor <skyup9393@gmail.com>
2023-11-28 00:55:06 +0900

    feat: wip-52 데이터 정합성 오류 알림 서비스 로직 추가

--- src/admin/dashboard/dashboard_service.py
+++ src/admin/dashboard/dashboard_service.py
@@ -1,8 +1,12 @@
 import logging
 from typing import List
 
-from schema.dashboard_response import AdminLogRes
-
+from schema.dashboard_response import (
+    AdminLogRes,
+    DuplicatedJurisdictionResSchema,
+    DuplicatedJurisdictionPoliticianResSchema,
+    IntegrityErrorRes,
+)
 
 logger = logging.getLogger("uvicorn")
 
@@ -25,3 +29,38 @@ async def get_admin_log(**kwargs) -> List[AdminLogRes]:
         }
         return_res.append(AdminLogRes(**data))
     return return_res
+
+
+async def get_integrity_error_data(**kwargs) -> IntegrityErrorRes:
+    politician_info_repo = kwargs["politician_info_repository"]
+    area_repo = kwargs["area_repository"]
+
+    duplicated_jurisdiction_data_list = area_repo.get_duplicated_jurisdiction()
+
+    duplicated_data = []
+    for constituency_data in duplicated_jurisdiction_data_list:
+        constituency_id = constituency_data[0]
+        constituency_data = area_repo.select_constituency_data_by_id(constituency_id)
+        politician_id_list = list(
+            map(
+                lambda x: x[0],
+                area_repo.select_politician_id_by_constituency_id(constituency_id),
+            )
+        )
+        politician_data_list = politician_info_repo.select_politician_data_by_id_list(
+            politician_id_list
+        )
+        politician_list = [
+            DuplicatedJurisdictionPoliticianResSchema(
+                id=politician[0].id, name=politician[0].name
+            )
+            for politician in politician_data_list
+        ]
+        data = {
+            "region": constituency_data.region,
+            "district": constituency_data.district,
+            "section": constituency_data.section,
+            "politician_list": politician_list,
+        }
+        duplicated_data.append(DuplicatedJurisdictionResSchema(**data))
+    return IntegrityErrorRes(duplicated_jurisdiction=duplicated_data)
