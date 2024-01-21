04b5b8149fb3f85a8d6083bb16d35262ef9d91e4
alsrlqor <skyup9393@gmail.com>
2024-01-21 23:38:15 +0900

    feat: wip-77 퍼블릭 웹용 조건 필터링 된 리스트 조회

--- src/public/public_router.py
+++ src/public/public_router.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List
+from typing import List, Optional
 
 from fastapi import APIRouter, Path, Depends, Query
 from starlette.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
@@ -8,17 +8,17 @@ from admin.politician.politician_service import PoliticianService
 from public.public_service import (
     get_public_constituency_data,
     get_public_politician_list,
+    get_public_politician_list_by_keyword
 )
 from repositories.area_repository import AreaRepository
 from repositories.politician_info_repository import PoliticianInfoRepository
-from schema.politician_response import GetSinglePoliticianDataRes
+from schema.politician_response import GetSinglePoliticianDataRes, GetPoliticianElementOfListRes
 from schema.public_response import (
     PublicConstituencyResSchema,
     PublicPoliticianListElementResSchema,
 )
 
 router = APIRouter()
-
 
 logger = logging.getLogger("uvicorn")
 
@@ -33,8 +33,8 @@ logger = logging.getLogger("uvicorn")
     summary="지역구 데이터 조회",
 )
 def get_constituency_public_handler(
-    region: str = Path(..., description="대분류 지역구(영문 소문자)"),
-    area_repository: AreaRepository = Depends(),
+        region: str = Path(..., description="대분류 지역구(영문 소문자)"),
+        area_repository: AreaRepository = Depends(),
 ) -> List[PublicConstituencyResSchema]:
     return get_public_constituency_data(region, area_repository)
 
@@ -49,12 +49,12 @@ def get_constituency_public_handler(
     summary="국회의원 데이터 초기 전체 목록 조회",
 )
 async def get_politician_list_public_handler(
-    assembly_term: int = Path(..., description="국회 회기"),
-    sort_type: str = Query("desc", description="정렬 기준(desc, asc)"),
-    page: int = Query(..., description="페이지네이션 0부터 시작"),
-    size: int = Query(default=10),
-    politician_info_repository: PoliticianInfoRepository = Depends(),
-    area_repository: AreaRepository = Depends(),
+        assembly_term: int = Path(..., description="국회 회기"),
+        sort_type: str = Query("desc", description="정렬 기준(desc, asc)"),
+        page: int = Query(..., description="페이지네이션 0부터 시작"),
+        size: int = Query(default=10),
+        politician_info_repository: PoliticianInfoRepository = Depends(),
+        area_repository: AreaRepository = Depends(),
 ) -> List[PublicPoliticianListElementResSchema]:
     return await get_public_politician_list(**locals())
 
@@ -69,7 +69,30 @@ async def get_politician_list_public_handler(
     summary="국회의원 데이터 개별 조회",
 )
 async def get_single_politician_public_handler(
-    politician_id: int = Path(..., description="의원 id"),
-    politician_service: PoliticianService = Depends(),
+        politician_id: int = Path(..., description="의원 id"),
+        politician_service: PoliticianService = Depends(),
 ) -> GetSinglePoliticianDataRes:
     return politician_service.get_politician_by_id(politician_id)
+
+
+@router.get(
+    "/filter/{assembly_term}",
+    status_code=HTTP_200_OK,
+    responses={
+        HTTP_200_OK: {"description": "Get politician search data"},
+        HTTP_400_BAD_REQUEST: {"description": "Bad request"},
+    },
+    summary="국회의원 데이터 조건별 검색 조회",
+    description="default: 공약 이행률 내림차순"
+)
+async def get_politician_list_by_keyword_public_handler(
+        assembly_term: int = Path(..., description="국회 회기"),
+        name: Optional[str] = Query(None, description="의원 이름"),
+        party: Optional[str] = Query(None, description="소속 정당"),
+        region: Optional[str] = Query(None, description="대분류 지역구 영문 소문자"),
+        page: int = Query(..., description="페이지네이션 0부터 시작"),
+        size: int = Query(default=10),
+        politician_info_repository: PoliticianInfoRepository = Depends(),
+        area_repository: AreaRepository = Depends()
+) -> List[PublicPoliticianListElementResSchema]:
+    return await get_public_politician_list_by_keyword(**locals())
--- src/public/public_service.py
+++ src/public/public_service.py
@@ -8,7 +8,7 @@ from common.enums import RegionType
 from database.connection import engine
 from database.models import Region, Constituency
 from repositories.area_repository import AreaRepository
-from schema.politician_response import JurisdictionResSchema
+from schema.politician_response import JurisdictionResSchema, GetPoliticianElementOfListRes
 from schema.public_response import (
     PublicConstituencyResSchema,
     PublicPoliticianListElementResSchema,
@@ -49,7 +49,7 @@ def get_constituency_data_from_db(region_name: str):
 
 
 def get_public_constituency_data(
-    region: str, area_repository: AreaRepository
+        region: str, area_repository: AreaRepository
 ) -> List[PublicConstituencyResSchema]:
     region_name = RegionType[region.upper()].value[1]
 
@@ -102,8 +102,8 @@ async def get_public_politician_list(**kwargs):
 
         if total_promise_count:
             politician_res.promise_execution_rate = (
-                completed_promise_count / total_promise_count
-            ) * 100
+                                                            completed_promise_count / total_promise_count
+                                                    ) * 100
         politician_res.constituency = jurisdiction_list
         return_res.append(politician_res)
 
@@ -114,5 +114,118 @@ async def get_public_politician_list(**kwargs):
         if x.promise_execution_rate is not None
         else float("-inf"),
         reverse=is_reverse,
+    )
+    return sorted_res
+
+
+async def get_public_politician_list_by_keyword(**kwargs) -> List[PublicPoliticianListElementResSchema]:
+    assembly_term = kwargs["assembly_term"],
+    name = kwargs["name"]
+    party = kwargs["party"]
+    region = kwargs["region"]
+    page = kwargs["page"]
+    size = kwargs["size"]
+    offset = page * size
+    politician_info_repo = kwargs["politician_info_repository"]
+    area_repo = kwargs["area_repository"]
+
+    return_res = []
+    if name or party:
+        politician_list = (
+            politician_info_repo.get_politician_search_data_for_admin(
+                offset=offset,
+                size=size,
+                assembly_term=assembly_term,
+                name=name,
+                party=party,
+            )
+        )
+
+        for politician in politician_list:
+            jurisdiction_data = (
+                area_repo.select_jurisdiction_data_by_politician_id(
+                    politician[0].id
+                )
+            )
+            jurisdiction_list = [
+                JurisdictionResSchema(
+                    id=data[0], region=data[1], district=data[2], section=data[3]
+                )
+                for data in jurisdiction_data
+            ]
+            politician_res = PublicPoliticianListElementResSchema.model_validate(
+                politician[0]
+            )
+
+            completed_promise_count = politician[0].completed_promise_count
+            total_promise_count = politician[0].completed_promise_count
+
+            if total_promise_count:
+                politician_res.promise_execution_rate = (
+                                                                completed_promise_count / total_promise_count
+                                                        ) * 100
+            politician_res.constituency = jurisdiction_list
+            return_res.append(politician_res)
+    else:
+        region_searched_keyword_replacements = {
+            "seoul": "서울",
+            "busan": "부산",
+            "incheon": "인천",
+            "daegu": "대구",
+            "gwangju": "광주",
+            "daejeon": "대전",
+            "ulsan": "울산",
+            "gyeonggi": "경기",
+            "sejong": "세종",
+            "jeju": "제주",
+        }
+
+        area = region_searched_keyword_replacements[region]
+        region_data = area_repo.get_region_data_by_random_text(area)
+
+        jurisdiction_politician_id_list = (
+            area_repo.select_jurisdiction_data_by_region_id_and_text(
+                offset, size, region_data[0]
+            )
+        )
+
+        for politician in jurisdiction_politician_id_list:
+            politician_id = politician[0]
+            politician_data = (
+                politician_info_repo.select_total_politician_data_by_id(
+                    politician_id
+                )
+            )
+            jurisdiction_data = (
+                area_repo.select_jurisdiction_data_by_politician_id(
+                    politician_id
+                )
+            )
+            jurisdiction_list = [
+                JurisdictionResSchema(
+                    id=data[0], region=data[1], district=data[2], section=data[3]
+                )
+                for data in jurisdiction_data
+            ]
+            politician_res = PublicPoliticianListElementResSchema.model_validate(
+                politician_data
+            )
+
+            completed_promise_count = politician_data.completed_promise_count
+            total_promise_count = politician_data.completed_promise_count
+
+            if total_promise_count:
+                politician_res.promise_execution_rate = (
+                                                                completed_promise_count / total_promise_count
+                                                        ) * 100
+            politician_res.constituency = jurisdiction_list
+            return_res.append(politician_res)
+
+    sorted_res = sorted(
+        return_res,
+        key=lambda x: x.promise_execution_rate
+        if x.promise_execution_rate is not None
+        else float("-inf"),
+        reverse=True,
     )
     return sorted_res
