71ebdb3c6f14a8426e7ec501f63736a8d695bca0
alsrlqor <skyup9393@gmail.com>
2024-01-22 21:48:49 +0900

    feat: wip-77 퍼블릭 리스트 필터링 검색 API url 수정

--- src/public/public_router.py
+++ src/public/public_router.py
@@ -43,26 +43,6 @@ def get_constituency_public_handler(
 
 
 @router.get(
-    "/politician/list/{assembly_term}",
-    status_code=HTTP_200_OK,
-    responses={
-        HTTP_200_OK: {"description": "Get politician data list"},
-        HTTP_400_BAD_REQUEST: {"description": "Bad request"},
-    },
-    summary="국회의원 데이터 초기 전체 목록 조회",
-)
-async def get_politician_list_public_handler(
-    assembly_term: int = Path(..., description="국회 회기"),
-    sort_type: str = Query("desc", description="정렬 기준(desc, asc)"),
-    page: int = Query(..., description="페이지네이션 0부터 시작"),
-    size: int = Query(default=10),
-    politician_info_repository: PoliticianInfoRepository = Depends(),
-    area_repository: AreaRepository = Depends(),
-) -> List[PublicPoliticianListElementResSchema]:
-    return await get_public_politician_list(**locals())
-
-
-@router.get(
     "/politician/single/{politician_id}",
     status_code=HTTP_200_OK,
     responses={
@@ -79,20 +59,20 @@ async def get_single_politician_public_handler(
 
 
 @router.get(
-    "/politician/list/filter/{assembly_term}",
+    "/politician/list/{assembly_term}",
     status_code=HTTP_200_OK,
     responses={
         HTTP_200_OK: {"description": "Get politician search data"},
         HTTP_400_BAD_REQUEST: {"description": "Bad request"},
     },
     summary="국회의원 데이터 조건별 검색 조회",
-    description="default: 공약 이행률 내림차순",
 )
 async def get_politician_list_by_keyword_public_handler(
     assembly_term: int = Path(..., description="국회 회기"),
     name: Optional[str] = Query(None, description="의원 이름"),
     party: Optional[str] = Query(None, description="소속 정당"),
     region: Optional[str] = Query(None, description="대분류 지역구 영문 소문자"),
+    sort_type: str = Query("desc", description="정렬 기준(desc, asc)"),
     page: int = Query(..., description="페이지네이션 0부터 시작"),
     size: int = Query(default=10),
     politician_info_repository: PoliticianInfoRepository = Depends(),
